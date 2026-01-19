5021db74bd1ef39e44913cf44979a0c7ed0722f3
sneha2004-alt <snehagworld@gmail.com>
2026-01-19 13:11:51 +0530

    code of the day javascript

---
+++ javascript/DataTypes.js
@@ -0,0 +1,33 @@
+var name ='Sneha';
+
+var surname ='gupta';
+
+var maried =false;
+
+var age = 99;
+
+var ans = `${name} ${surname} ${maried} ${age}`;//string backtick
+
+
+//console.log(typeof name , surname typye of marieed ,age);
+
+//console.log(ans);
+//console.log(type  of ans);
+
+for(let i=0; i<3;i++){
+    console.log(i);
+}
+// console.log(i);
+
+const val=3;
+
+val='gupta';
+console.log(val);
+
+
+
+
+
+console.log(place);
+
+let place ='boisar';
