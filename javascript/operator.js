12ca0e20ec9426c6acb7b2e39999fe0873e30040
sneha2004-alt <snehagworld@gmail.com>
2026-01-28 12:32:00 +0530

    code of the day

---
+++ javascript/operator.js
@@ -0,0 +1,66 @@
+//assignment operators
+let count =0;
+let name='Sneha';
+
+count += 4; //count = count+4
+count -= 2; //count = count -4
+count *= 2; //count =count*4
+count /= 2; //count = count/4
+
+console.log(count);
+//arithmatic operators[[maths -> sign's]]
+
+let a = 6;
+let b = 9;
+
+let sum = a+b;
+console.log(sum);
+let sub = a-b;
+console.log(sub);
+let mul = a*b;
+console.log(mul);
+let div = a/b;
+console.log(div);
+let mod= a % b;
+console.log(mod);
+let expo= a ** b;
+console.log(expo);
+/*+
+-
+*
+/
+%
+**
+*/
+
+
+
+//concatination  operators
+let name1='Sneha';
+let name2='Gupta';
+
+// let fullname = '\n'+name1+ '\n' +name2;
+// console.log(fullname);
+
+let fullName = `${name1} ${name2}`;
+console.log(fullName);
+
+name1=name1 + ' ' + name2;
+
+
+//name1 += '' + name2;
+console.log(name1);
+
+
+
+//conditional operators
+
+//ternary operators
+
+
+//unary operators
+
+//types of operators
+
+
+//binary conversion
