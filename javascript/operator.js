bb63c4c928c749db3c717a9de547a19016fbb30a
sneha2004-alt <snehagworld@gmail.com>
2026-01-29 13:08:52 +0530

    code of the day

--- javascript/operator.js
+++ javascript/operator.js
@@ -51,18 +51,65 @@ name1=name1 + ' ' + name2;
 //name1 += '' + name2;
 console.log(name1);
 
+//conditional/coomparison operators-
 
+//LOGICAL OPERATOR 
+let flag1 = true;
+let flag2 = false;
+let flag3 = false;
 
-//conditional operators
+console.log(true || false);
+console.log(!flag3);
 
+let value ='null';
+let ans = value ?? 50;
 
+console.log(ans)
 
 //ternary operators
+ let flag  = false;
 
+ let ans1  = flag ? 'i am in' : 'i am out';
+ console.log(ans1)
 
 //unary operators
+let name4='5';
+
+name4 = +name4;
+console.log(+name4);
 
 //types of operators
+console.log(typeof name4);
+
+//conversion between string to number || number to string
+
+let xyz = '123';
+console.log(+xyz);
+console.log(typeof +xyz);
+
+//method
+
+let ans55 =parseInt(xyz);
+console.log(ans55);
+console.log(typeof ans55);
+//constructor method
+let ans56 =Number(xyz);
+console.log(ans55);
+console.log(typeof ans56);
+
+// number to string 
+let numbertoString = 56;
+
+console.log();
+
+const jugad = numbertoString + '';
+console.log('jugad:ans',jugad,'type:',typeof jugad);
+
+const jugad_2 = numbertoString.toString();
+console.log('jugad_2:ans',jugad_2,'type:',typeof jugad_2);
+
+const jugad_3 = numbertoString;
+console.log('jugad_3:ans',jugad_3,'type:',typeof jugad_3);
 
 
 //binary conversion
