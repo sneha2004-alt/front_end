cffa36bd19739d0a8148e898c5bb7acc9d40e8d3
sneha2004-alt <snehagworld@gmail.com>
2026-02-02 13:17:08 +0530

    code of the day

---
+++ javascript/bitwise.js
@@ -0,0 +1,9 @@
+console.log(50>>2)/*right shift*/
+console.log(50<<2)/*left shift*/
+
+console.log(20>>2)/*right shift*/
+console.log(20<<2)/*left shift*/
+
+console.log(40>>2)/*right shift*/
+console.log(40<<2)/*left shift*/
+
