abbb6eadf2f13065f17c60d34cc5aeb45c3240bc
sneha2004-alt <snehagworld@gmail.com>
2026-02-06 12:58:15 +0530

    code of the day

---
+++ javascript/if_else.js
@@ -0,0 +1,78 @@
+// let age = +process.argv[2];
+// // let age = prompt("enter you are age");
+
+// //above 60
+// let value = age >=18?console.log('adult'):console.log('kid');
+
+// if (age >=18 && age <60){
+//    //block of code
+//    console.log('adult');
+// }else if (age>60){
+//     console.log('old');
+// }else{
+//     //block of code
+//     console.log('kid');
+// }
+
+//question 1
+// let num = 14;
+
+// if (num % 2 === 0) {
+//     console.log("Even number");
+// } else {
+//     console.log("Odd number");
+// }
+
+//question 2
+// let num = 8;   // change this value
+
+// if (num > 0) {
+//     console.log("Number is Positive");
+// } 
+// else if (num < 0) {
+//     console.log("Number is Negative");
+// } 
+// else if (num === 0) {
+//     console.log("Number is Zero");
+// }
+
+
+
+// // question 3 if + comparison operator 
+// let a = 10;
+// let b = 20;
+
+// if (a > b) {
+//     console.log(a + " is greater");
+// } else {
+//     console.log(b + " is greater");
+// }
+
+
+
+//question 4  
+// let a=20;
+// let b=30;
+// let c=40;
+// if (a >= b && a >= c) {
+//     console.log(a + " is largest");
+// } 
+// else if (b >= a && b >= c) {
+//     console.log(b + " is largest");
+// } 
+// else {
+//     console.log(c + " is largest");
+// }
+
+//question 5
+let year = 2025;
+
+let isLeap =
+  (year % 400 === 0) ||
+  (year % 4 === 0 && year % 100 !== 0);
+
+if (isLeap) {
+  console.log(year + " is a Leap Year");
+} else {
+  console.log(year + " is NOT a Leap Year");
+}
