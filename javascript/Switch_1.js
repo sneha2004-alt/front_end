3ef2121c3c8f2394b14e737fcc5319802061b82f
sneha2004-alt <snehagworld@gmail.com>
2026-02-09 11:56:36 +0530

    code of the day

---
+++ javascript/Switch_1.js
@@ -0,0 +1,14 @@
+const value = undefined;
+
+switch (null){
+    case true:
+    console.log('yes');
+    break;
+
+    case false:
+        console.log('no');
+        break;
+
+        default:
+            console.log('please select boolean value true/false');
+}
