b386d1b80a0eac3b3a42b8f69535a935730df006
sneha2004-alt <snehagworld@gmail.com>
2026-02-09 13:01:13 +0530

    code of the day

--- javascript/Switch_1.js
+++ javascript/Switch_1.js
@@ -12,3 +12,29 @@ switch (null){
         default:
             console.log('please select boolean value true/false');
 }
+
+
+
+
+
+
+
+//function's
+/*
+define
+name-convention
+call
+ */
+function sumofvalue(a,b,c){
+    console.log(a + b + c);
+    return a + b + c;
+}
+
+console.log(sumofValue(10,7,3));
+
+const aarrowfunc = (a,b,c) =>  {
+    return a + b + c
+}
+
+//const ans = arrowfunc(1,2,3);
+console.log(arrowfunc(1,2,3));
