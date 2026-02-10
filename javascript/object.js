70ab48619d5b0f30016b11f0d682bdc1463bb4a1
sneha2004-alt <snehagworld@gmail.com>
2026-02-10 12:59:30 +0530

    code of the day

---
+++ javascript/object.js
@@ -0,0 +1,20 @@
+let name = 'khan';
+console.log(name);
+
+let obj={
+    name: 'Sneha',
+};
+
+//read
+console.log(obj.name);
+
+//create
+obj.age = 99;
+console.log(obj);
+
+// update
+obj.age =102;
+
+delete obj.age;
+
+console.log(obj);
