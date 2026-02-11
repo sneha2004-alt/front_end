9abf9ca5f61629f0cbbac83a58681232e83644a9
sneha2004-alt <snehagworld@gmail.com>
2026-02-11 13:13:49 +0530

    code of the day

---
+++ javascript/loop.js
@@ -0,0 +1,58 @@
+// // //(1) print number from  1-10 using a LOOP//
+// let arr = [];
+// for (let i = 1; i <= 10; i++) {
+//   arr.push(i);
+// }
+
+// for (let j = 0; j < arr.length; j++) {
+//   console.log(arr[j]);
+// }
+
+// // (2)print all even number number between 1 and 50.& // (3)print all odd number between 1 and 50
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`this is even number :-${1}`);
+//   } else {
+//     console.log(`this is odd number :-${i}`);
+//   }
+// }
+
+// // (4)print the multiplication table of 5.
+// for (let i = 1; i <= 20; i++) {
+//   console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+// // (5)print number from 100 down to 1(reverse order).
+// for (let i = 20; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// // (6)print the sum of numbers from 1 to 100.
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum = sum + i;
+// }
+
+// console.log("Sum from 1 to 100 =", sum);
+
+// // (7)print the square   of the first 10 natural number.
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`square of ${i}=${i * i}`);
+// }
+// // (8)print the cube of the first 10 natural nnumber
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`cube of ${i}=${i * i * i}`);
+// }
+// // (9)print the first 10 multiplies of 3.
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`3x ${i} = ${3 * i}`);
+// }
+
+// (10)print the factorial of a given number.
+let num = +process.argv[2];
+let fact=1
+for (i = 1; i <= num; i++) {
+ fact=fact * i;
+}
+console.log(fact)
--- javascript/object.js
+++ javascript/object.js
@@ -18,3 +18,61 @@ obj.age =102;
 delete obj.age;
 
 console.log(obj);
+
+// CRUD
+//CREATE
+//READ
+//UPDATE
+//DELETE
+
+//LOOPING
+
+let array = [
+    1,
+    2,
+    true,
+    false,
+    {name:'sneha'},
+    ['abhishek','kashish'],
+    'renee',
+];
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+
+//for("initaialization","condition","increment++" / decrement--"){
+//code of output//}
+for(let i=0; i<array.length; i++){
+    console.log(
+        `\n\ this value :${array[i]} \n\  \n\ this is index : ${i}`);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
