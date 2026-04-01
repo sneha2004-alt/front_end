e666e1ed278d71add4c4cc61aaddc38c2b6b3692
sneha2004-alt <snehagworld@gmail.com>
2026-04-01 13:16:31 +0530

    code of the day

---
+++ javascript/import_export/Export/navbar.js
@@ -0,0 +1,26 @@
+export const Navbar =() => {
+    return`<nav
+    style="display:flex;justify-content:space-evenly;align-items:center">
+    <div>
+    <img
+     width="50"
+     style="border-radius:50%"
+     src="C:\Users\SNEHA GUPTA\OneDrive\Desktop\front_end\javascript\import_export\cat-7287671_1280.jpg"
+     alt="logo"/>
+     </div>
+     
+     <div style="display:flex;gap:1rem">
+     <span>home</span>
+     <span>about</span>
+     <span>contact</span>
+     </div>
+     
+     <div>
+     <button>login</button>
+     </div>
+     </nav>`;
+
+};
+
+
+const NavStyle=()
---
+++ javascript/import_export/Export/sum.js
@@ -0,0 +1,16 @@
+const sumValue =(a,b) => {
+    return a + b;
+};
+const subValue =(a,b)=>{
+    return a-b;
+};
+
+export {sumValue,subValue};
+//document.queryselector('#sum').innerHTML =`sum of given value: $ {ans}`;
+
+//export
+/*
+
+name export{sumValue}
+default export -> xyz
+*/
