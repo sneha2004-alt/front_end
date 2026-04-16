8dfb9b570246d14d678b11df70f3dc311035438f
sneha2004-alt <snehagworld@gmail.com>
2026-04-16 13:06:21 +0530

    code of the day

---
+++ javascript/constructor/index.js
@@ -0,0 +1,23 @@
+/*keyword 
+
+new -> create new Object
+
+this -> point created object
+*/
+
+
+class Car{
+    constructor(name,age){
+        ((this.name = name),(this.life = age));
+    }
+}
+
+
+
+
+// function Car(name,age){
+//     ((this.name = name),(this.age = age));
+
+// }
+// let c1 = new Car('maruti',18);
+// console.log(c1);
